7b4d2ff650ae913d3b083c34a54aafa6945a6603
jini <dbwls20202554@gmail.com>
2024-06-05 16:47:51 +0900

    다리놓기 추가

--- src/BOJ/DynamicProgramming/Bridge.java
+++ src/BOJ/DynamicProgramming/Bridge.java
@@ -7,8 +7,9 @@ import java.util.StringTokenizer;
 
 //1010번
 public class Bridge {
-    //파스칼의 삼각형 ??
-    //n-1Cr-1 + n-1Cr = nCr
+    //조합을 써야 한다! --> 중복 허용 X기 때문에 겹치는 경우의 수 세지 않음
+    //파스칼의 삼각형 ?? 대체 뭐임??
+    //n-1Cr-1 + n-1Cr = nCr (삼각형에서 윗 자리 두개 더하면 내 값이 나옴)
     //그냥 조합 방식 쓰면 계산량 너무 많아짐 (시간 초과) --> 파스칼의 삼각형 공식으로 쪼개서 반복문 돌리기
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,14 +23,14 @@ public class Bridge {
             dp[i][i] = 1;
         }
 
-        //파스칼 삼각형 공식
+        //파스칼 삼각형 만들기
         for (int i = 2; i < 30; i++) {
             for (int j = 1; j < i; j++) {
                 dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
             }
         }
 
-        //N,M 입력 받기
+        //N,M 입력 받아 원하는 인덱스에서 값 뽑아오기
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
